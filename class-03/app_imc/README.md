9d292b5e74ec8d878b98d57842c6da7171b8b080
paulolopestech <phlj.junior@gmail.com>
2023-09-01 23:31:59 -0300

    :books: update doc

--- class-03/app_imc/README.md
+++ class-03/app_imc/README.md
@@ -1,3 +1,18 @@
 # Calculadora de IMC
 
 - Calculadora de IMC desenvolvida durante as aulas práticas da disciplina de Engenharia de Software, ministrada pelo monitor Itamar.
+- Após inserir os dados, será mostrado um resultado indicando se você está ou não acima do peso.
+
+
+## Instalação e Execução:
+
+Certifique-se de ter o node.js instalado e instale as dependencias do projeto com o comando:
+
+    npm install
+
+Na raiz do projeto, execute o seguinte comando para execução do projeto:
+
+
+    npm run dev
+
+Após isso, acesse [localhost:8080](http://localhost:8080) para visulaizar a aplicação
--- class-03/app_imc/src/App.svelte
+++ class-03/app_imc/src/App.svelte
@@ -4,6 +4,7 @@
 	let altura = 0;    // Altura do usuário
 	let imc = 0;       // Índice de Massa Corporal
 	let result = '';
+	let classificationColor = '';
   
 	// Função para calcular o IMC
 	function calcularIMC() {
@@ -18,16 +19,22 @@
 	function setResult(imc) {
 		if(imc < 18.5) {
 			result = 'abaixo do peso';
+			classificationColor = 'red';
 		} else if (imc > 18.6 && imc < 24.9) {
 			result = 'Peso ideal (parabéns)';
+			classificationColor = 'green';
 		} else if(imc > 25.0 && imc < 29.9) {
 			result = 'Levemente acima do peso';
+			classificationColor = 'yellow';
 		}else if(imc > 30.0 && imc < 34.9) {
 			result = 'Obesidade grau I';
+			classificationColor = 'orange';
 		}else if(imc > 35.0 && imc < 39.9) {
 			result = 'Obesidade II (severa)';
+			classificationColor = 'red';
 		} else {
 			result = 'Obesidade III (mórbida)';
+			classificationColor = 'red';
 		}
 	}
   </script>
@@ -42,7 +49,7 @@
 	</label>
 	{#if imc > 0}
 	  <p>Seu IMC é {imc}</p>
-	  <p>Sua classificação é: {result}</p>
+	  <p>Sua classificação é: <b class={classificationColor}>{result}</b></p>
 	{/if}
   </main>
   
@@ -50,9 +57,13 @@
 	main {
 	  font-family: sans-serif;
 	  text-align: center;
-	  padding: 2rem;
+	  width: 95vw;
+	  margin: 0 auto;
+	  vertical-align: middle;
+	  position: absolute;
+	  top: 40%;
+	  transform: translateY(-50%);
 	}
-  
 	h1 {
 	  font-size: 1.5rem;
 	  margin-bottom: 1rem;
@@ -65,6 +76,19 @@
   
 	input {
 	  padding: 0.25rem;
+	}
+
+	.red {
+		color: red;
+	}
+	.green {
+		color: green;
+	}
+	.orange {
+		color: orange;
+	}
+	.yellow {
+		color: yellow;
 	}
   </style>
   
